12bed140f26f6fa06a945d0183e1b9c3a9f2a937
vcheshko <vladimircheshko@gmail.com>
2018-03-11 18:33:33 +0200

    added net core MVC template

---
+++ VideoJs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoJs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
